09f52714f5e268147a36cff41afd8b6c4c871c6c
Žilvinas Rudžionis <zilvinas.rudzionis@adform.com>
2017-04-21 09:46:24 +0300

    Start creating notifications

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule,
