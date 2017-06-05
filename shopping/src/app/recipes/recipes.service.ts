27dbd314b2e11667ae185abecb8e921ebc63ebda
Žilvinas Rudžionis <zilvinas.rudzionis@adform.com>
2017-06-05 23:49:23 +0300

    Add ingredients to recipe model

--- shopping/src/app/recipes/recipes.service.ts
+++ shopping/src/app/recipes/recipes.service.ts
@@ -1,14 +1,28 @@
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+
 
 export class RecipesService {
   recipes: Recipe[] = [
-    new Recipe('First recipe', 'First recipe description', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-    new Recipe('Second recipe', 'Second recipe description', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+    new Recipe('First recipe',
+    'First recipe description',
+    'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+    [
+      new Ingredient('apple', 2),
+      new Ingredient('pineapple', 3),
+    ]),
+    new Recipe('Second recipe',
+    'Second recipe description',
+    'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+    [
+      new Ingredient('vodka', 1),
+      new Ingredient('meat', 5),
+    ])
   ];
   selectedRecipe: Recipe;
 
   selectRecipe(recipe: Recipe) {
     this.selectedRecipe = recipe;
   }
-  
+
 }
